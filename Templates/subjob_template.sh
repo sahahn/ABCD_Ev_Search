ec782de8ec7124d5a5f3a216df2d21a0d4f39256
sahahn <sahahn@uvm.edu>
2019-05-22 16:58:53 -0400

    Maybe ?

--- Templates/subjob_template.sh
+++ Templates/subjob_template.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export PATH="/users/s/a/sahahn/scratch/anaconda3/bin:$PATH"
 
-echo $PBS_TASKNUM
-
 cd EV_SEARCH_LOCATION
 python Search.py REPLACE$(($PBS_TASKNUM-2+START_NUM)).pkl LOAD "CONFIG_LOC"
